326482e6504024daf2f452cb269bdc97a35682f4
Rondy Kafumu <rondykafumu76@gmail.com>
2026-05-21 17:22:11 +0100

    config app uri

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_item():
+    return {"data": 
+            {
+                "name":"sarthak"
+                }
+            }
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/hello")
+def read_item():
+    return {"data": 
+            {
+                "name":"rondy"
+                }
+            }
