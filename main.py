8d9811d553030c402dd1c86f7ddcbae19e18caac
Rondy Kafumu <rondykafumu76@gmail.com>
2026-05-27 22:49:45 +0100

    added blog file

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
 
 
 @app.get("/blog")
-def read_item(limit,published):
+def read_item(limit=10,published:bool=True,sort: Optional[str]=None):
+    if published:
+        return {
+            "data": f"{limit} published blogs from the db"
+        }
+    else:
+        return {
+            "data": f"{limit} blogs from the db"
+        }
 
-    if published == False:
-        return {"data": 
-                {
-                    "name":f'{limit} sarthak'
-                }
-            }
-    elif published == True:
-        return {'data':"error"}
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post("/blogModel")
+def read_item(blog:Blog):
+    
+    return {
+        "Name": f"Blog is created with title as {blog.title}",
+        "body": f"Blog is created with body as {blog.body}",
+        "published": f"Blog is created with published as {blog.published}"
+    }
 
 @app.get("/hello/{id}")
 def read_item(id):
@@ -35,3 +51,6 @@ def read_item(id:int):
             }
             
     }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
